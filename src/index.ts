0433712f3209a8204f1c823123bf867414cc189e
alessandro-corra <alessandro.corra@2132.io>
2023-05-01 21:33:24 +0100

    [develop] - add nodemon configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
 const port = 3000;
 
-server.get('/', (req, res ) => {
-	console.log(req);
-	res.send('all is working');
+server.get('/', (req: Request, res: Response ) => {
+	res.status(200).send('all is working');
 })
 
 server.listen(port, () => {
