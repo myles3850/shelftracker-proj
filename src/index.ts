50b5d612638fc582b7fd802cac3c0e1a5c1ede27
Myles Browne <myles@browne.link>
2023-05-06 11:45:01 +0100

    created db connection

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,30 @@ dotenv.config();
 
 import "reflect-metadata"
 import express, { Request, Response } from 'express';
+import { DataSource } from 'typeorm';
+
+import { Book } from './entities/book';
 
 const server = express();
 const port = 3000;
+
+const AppDBSource = new DataSource({
+	type: "mysql",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [Book],
+    logging: false,
+})
+
+try{
+	AppDBSource.initialize()
+}
+catch(error) {
+	throw error;
+}
 
 server.get('/', (req: Request, res: Response ) => {
 	res.status(200).send('all is working');
