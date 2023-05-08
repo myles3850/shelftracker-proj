b3da2ec3fb7a790327053fabc59013afde07f1dc
alessandro-corra <alessandro.corra@2132.io>
2023-05-08 18:30:45 +0100

    generated validation

---
+++ src/dtos/book.ts
@@ -0,0 +1,27 @@
+import { EBookTypes } from "../enums";
+
+import {IsString, MaxLength, IsInt, IsNotEmpty, IsPositive, IsEnum} from 'class-validator'
+import { IBookRequest } from "../interfaces";
+
+export class BookDTO {
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  title: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @IsPositive()
+  pages: number;
+
+  @IsNotEmpty()
+  @IsEnum(EBookTypes)
+  type: EBookTypes;
+
+  constructor(body: IBookRequest) {
+    this.title = body.title;
+    this.pages = body.pages;
+    this.type = body.type;
+  }
+}
