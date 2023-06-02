99335d6c0ccda7948816ee2ffdc0e2ede9cc254b
Myles Browne <myles@browne.link>
2023-06-02 23:20:44 +0100

    remove unused imports

--- src/entities/subGenre.ts
+++ src/entities/subGenre.ts
@@ -1,5 +1,4 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { Book } from './book';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'sub_genre' })
 export class SubGenre {
