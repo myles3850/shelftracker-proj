4267f3ddc52cb14763ceb9743469bf57c4469d3b
Myles Browne <myles@browne.link>
2023-06-15 22:20:05 +0100

    added minio storage

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -1,24 +1,26 @@
+import crypto from 'crypto';
 import express, { Request, Response } from 'express';
 import multer from 'multer';
-import path from 'path';
+import { addImageToBucket, insertImageReference } from '../services';
 
 export const imageRouter = express.Router();
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 5_000_000 } }); //5MB filesize
 
-
-
-
 imageRouter.post('/',upload.single('image'), async(req: Request, res: Response): Promise<any> =>{
- const { originalname, buffer } = req.file;
+ const { buffer, mimetype } = req.file;
+ const filename = crypto.randomUUID();
+
+ const fileExtension = mimetype.split('/')[1];
+
+ const { S3_ADDRESS, S3_PORT, S3_BUCKET } = process.env;
+ const imageURL = `http://${ S3_ADDRESS }:${ S3_PORT }/${ S3_BUCKET }/${ filename }.${ fileExtension }`;
 
 	try {
-		const filePath = path.join(destinationPath, originalname);
-		res.send();
+		await addImageToBucket(`${ filename }.${ fileExtension }`, buffer, mimetype);
+		const imageId = await insertImageReference(imageURL);
+		res.status(201).send({ imageId });
 	} catch (error) {
-		console.log(error)
-		res.send();
+		res.status(500).send(error.message);
 	}
-
-
 
 });
